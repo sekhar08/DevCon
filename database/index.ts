cc14d43e9cd7d65d5e8e451498d5f178fa49ce86
sekhar08 <sekhargandrotula@gmail.com>
2026-02-17 01:06:58 +0530

    add database models

---
+++ database/index.ts
@@ -0,0 +1,5 @@
+// Export Event model and interface
+export { default as Event, IEvent } from './event.model';
+
+// Export Booking model and interface
+export { default as Booking, IBooking } from './booking.model';
